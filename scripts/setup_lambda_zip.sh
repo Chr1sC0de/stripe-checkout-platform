6f0df87176002ed4679156e26127d8389b6551b2
Chris Mamon <chrisam1993@gmail.com>
2024-07-24 22:38:49 +1000

    feature: added comments to setup_lambda

--- scripts/setup_lambda_zip.sh
+++ scripts/setup_lambda_zip.sh
@@ -4,6 +4,8 @@ create a zip for the api library, this should be run after
     /scripts/initialize_backend_python_environment.sh
 '
 
+echo "INFO: Setting up lambda function"
+
 if [[ -n $GITHUB_ACTIONS ]]; then
     source $HOME/.cargo/env
     source "$HOME/.rye/env"
@@ -17,6 +19,8 @@ cd "$source_folder"/../backend/api-lib || exit
 . .venv/bin/activate
 
 rye sync
+
+echo "INFO: Creating zip"
 
 python -m pip install . -t lib
 
@@ -36,3 +40,11 @@ zip dist/lambda.zip -u api.py
 chmod 775 dist/lambda.zip
 
 rm -rf lib
+
+echo "INFO: Finished creating zip"
+
+deactivate
+
+cd $source_folder/.. || exit
+
+echo "INFO: Finished setting up lambda function"
