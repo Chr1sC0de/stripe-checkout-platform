e78f2dda90bec2dceb7d04402f6eb31575bed4b0
Chris Mamon <chrisam1993@gmail.com>
2024-07-27 11:55:49 +1000

    feature: added endpoint for local development

--- backend/infrastructure/infrastructure/stacks/fastapi_lambda.py
+++ backend/infrastructure/infrastructure/stacks/fastapi_lambda.py
@@ -89,7 +89,7 @@ class InfrastructureStack(Stack):
             auth_type=L.FunctionUrlAuthType.NONE,
             # cross origin resource sharing
             cors=L.FunctionUrlCorsOptions(
-                allowed_origins=["*"],
+                allowed_origins=["https://0.0.0.0:3000"],
                 allowed_methods=[L.HttpMethod.ALL],
                 allowed_headers=["*"],
             ),
