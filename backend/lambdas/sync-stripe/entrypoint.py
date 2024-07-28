9223f1de9a9642dc325fd41e7bc1e9ed7331adcd
Chris Mamon <chrisam1993@gmail.com>
2024-07-28 13:35:37 +1000

    feature: added reponse status

--- backend/lambdas/sync-stripe/entrypoint.py
+++ backend/lambdas/sync-stripe/entrypoint.py
@@ -1,9 +1,15 @@
 from sync_stripe import utils
 
+
 def handler(event, context):
     main()
-    return
+    return {"message": "completed successfully"}
+
 
 def main():
     utils.sync_completed_checkout_sessions()
     utils.sync_product_price_customer_table()
+
+
+if __name__ == "__main__":
+    handler((), {})
