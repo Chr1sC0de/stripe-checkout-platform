1074c0061aac219bc692ed0c1a0a2d81bfab0ced
Chris Mamon <chrisam1993@gmail.com>
2024-08-06 00:59:32 +1000

    feature: added ecr creation

---
+++ backend/infrastructure/infrastructure/stacks/ecr.py
@@ -0,0 +1,19 @@
+from aws_cdk import Stack
+from aws_cdk import aws_ecr as ecr
+from constructs import Construct
+from infrastructure import utils
+
+
+class InfrastructureStack(Stack):
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        **kwargs,
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        repository = ecr.Repository(
+            self,
+            f"{utils.COMPANY}-{utils.DEVELOPMENT_ENVIRONMENT}",
+            empty_on_delete=true,
+        )
