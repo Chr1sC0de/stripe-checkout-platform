d38400a5fdeab132392e146fa0687377720295da
Chris Mamon <chrisam1993@gmail.com>
2024-08-06 23:27:14 +1000

    fix: updated naming convention

--- backend/infrastructure/infrastructure/stacks/ecr.py
+++ backend/infrastructure/infrastructure/stacks/ecr.py
@@ -1,8 +1,9 @@
-from aws_cdk import Stack, RemovalPolicy
+from aws_cdk import RemovalPolicy, Stack
 from aws_cdk import aws_ecr as ecr
-from constructs import Construct
-from infrastructure import utils
 from aws_cdk import aws_ssm as ssm
+from constructs import Construct
+
+from infrastructure import utils
 
 
 class InfrastructureStack(Stack):
@@ -29,8 +30,9 @@ class InfrastructureStack(Stack):
             self,
             "ssm_ect_general_repository_name",
             parameter_name=f"/{utils.COMPANY}/{utils.DEVELOPMENT_ENVIRONMENT}/ecr-general-repository-name",
-            string_value=repository.repository_uri,
+            string_value=repository.repository_name,
         )
+
         ssm.StringParameter(
             self,
             "ssm_ect_general_repository_uri",
