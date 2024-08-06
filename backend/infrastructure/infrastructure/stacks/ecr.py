30fae64adf40df18b12545973afdce4481c5859d
Chris Mamon <chrisam1993@gmail.com>
2024-08-06 23:23:05 +1000

    feature: added ssm string parameters to the ecr infrastructure stack

--- backend/infrastructure/infrastructure/stacks/ecr.py
+++ backend/infrastructure/infrastructure/stacks/ecr.py
@@ -2,6 +2,7 @@ from aws_cdk import Stack, RemovalPolicy
 from aws_cdk import aws_ecr as ecr
 from constructs import Construct
 from infrastructure import utils
+from aws_cdk import aws_ssm as ssm
 
 
 class InfrastructureStack(Stack):
@@ -13,7 +14,7 @@ class InfrastructureStack(Stack):
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        ecr.Repository(
+        repository = ecr.Repository(
             self,
             "General",
             empty_on_delete=True if ("dev" in utils.DEVELOPMENT_ENVIRONMENT) else False,
@@ -22,4 +23,17 @@ class InfrastructureStack(Stack):
                 if ("dev" in utils.DEVELOPMENT_ENVIRONMENT)
                 else RemovalPolicy.RETAIN
             ),
+        )
+
+        ssm.StringParameter(
+            self,
+            "ssm_ect_general_repository_name",
+            parameter_name=f"/{utils.COMPANY}/{utils.DEVELOPMENT_ENVIRONMENT}/ecr-general-repository-name",
+            string_value=repository.repository_uri,
+        )
+        ssm.StringParameter(
+            self,
+            "ssm_ect_general_repository_uri",
+            parameter_name=f"/{utils.COMPANY}/{utils.DEVELOPMENT_ENVIRONMENT}/ecr-general-repository-uri",
+            string_value=repository.repository_uri,
         )
